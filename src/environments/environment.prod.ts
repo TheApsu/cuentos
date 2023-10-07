2d8f3c331a38a5c7e5125da74b82754460d3c3ea
Ricardo Duque <theapsu@gmail.com>
2023-10-07 10:58:47 -0400

    new base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,12 +5,14 @@ export const environment = {
   // baseUrl: '/assets/db/db.json',
   cuentosUrl: 'https://theapsu.github.io/cuentos/cuentos',
   // apiUrl: 'http://localhost:3000'
-  apiUrl: 'https://node-js-mysql-railway-production.up.railway.app',
-  
-  paypalKey: 'AUUmKM0vHKSy4rHAxKFaUti0btItQbLpw6GC2CNGnX__eXqeE896gUaV0cv2I9B-qldP6Xa7QhSf-RAJ',
-  paypalSecretKey: 'EJDfqoMh3sBxS0bkYSC2nF7uNAkVP26ZO32nU5ApBzWTOOHosfBcC6R2j9mgmBaHxU8IMDzSg949eT9Q',
+  apiUrl: 'https://nodejs-railway-server.onrender.com',
+
+  paypalKey:
+    'AUUmKM0vHKSy4rHAxKFaUti0btItQbLpw6GC2CNGnX__eXqeE896gUaV0cv2I9B-qldP6Xa7QhSf-RAJ',
+  paypalSecretKey:
+    'EJDfqoMh3sBxS0bkYSC2nF7uNAkVP26ZO32nU5ApBzWTOOHosfBcC6R2j9mgmBaHxU8IMDzSg949eT9Q',
 
   paypalPlan: 'P-4BC78204AU2233620MRCEPPY',
-  accessToken: 'A21AAPQndQzsHHaSsslKzzTTfGGEr2aguZLfHtp3XVKqSnskpKm0-RRWCmG2X1O_9GxVtyJlz4a2P9geNB5KJPO46Q60JJ8_Q'
-
+  accessToken:
+    'A21AAPQndQzsHHaSsslKzzTTfGGEr2aguZLfHtp3XVKqSnskpKm0-RRWCmG2X1O_9GxVtyJlz4a2P9geNB5KJPO46Q60JJ8_Q',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,20 +6,23 @@ export const environment = {
   production: false,
   baseUrl: 'https://theapsu.github.io/cuentos/cuentos',
   // baseUrl: '/assets/db/db.json',
-  
+
   cuentosUrl: 'https://theapsu.github.io/cuentos/cuentos',
 
   // apiUrl: 'http://localhost:3000',
-  apiUrl: 'https://node-js-mysql-railway-production.up.railway.app',
+  apiUrl: 'https://nodejs-railway-server.onrender.com',
   // paypalKey: 'AUUmKM0vHKSy4rHAxKFaUti0btItQbLpw6GC2CNGnX__eXqeE896gUaV0cv2I9B-qldP6Xa7QhSf-RAJ',
   // paypalPlan: 'P-4BC78204AU2233620MRCEPPY'
   // accessToken: 'A21AAPQndQzsHHaSsslKzzTTfGGEr2aguZLfHtp3XVKqSnskpKm0-RRWCmG2X1O_9GxVtyJlz4a2P9geNB5KJPO46Q60JJ8_Q'
 
-  paypalKey: 'AepZr9uiwtcTOVyqrx5zdokXhvt3hiRdLUhY1A81N6lWiA8kkOOI-7K-JgaKGZ8D2Wgi9Sb-vehGlcKA',
-  paypalSecretKey: 'EJDfqoMh3sBxS0bkYSC2nF7uNAkVP26ZO32nU5ApBzWTOOHosfBcC6R2j9mgmBaHxU8IMDzSg949eT9Q',
-  
+  paypalKey:
+    'AepZr9uiwtcTOVyqrx5zdokXhvt3hiRdLUhY1A81N6lWiA8kkOOI-7K-JgaKGZ8D2Wgi9Sb-vehGlcKA',
+  paypalSecretKey:
+    'EJDfqoMh3sBxS0bkYSC2nF7uNAkVP26ZO32nU5ApBzWTOOHosfBcC6R2j9mgmBaHxU8IMDzSg949eT9Q',
+
   paypalPlan: 'P-04W43159W81552346MRCGS4A',
-  accessToken: 'A21AALl2zBcjAq7b0bquHKG6kkdWqYiRtJ0HpQHGeW-wz5DXio1WNiHzk6_FOrNiuy3HSPMYROK30S2Rttn32EdWgRQeCXt4w'
+  accessToken:
+    'A21AALl2zBcjAq7b0bquHKG6kkdWqYiRtJ0HpQHGeW-wz5DXio1WNiHzk6_FOrNiuy3HSPMYROK30S2Rttn32EdWgRQeCXt4w',
 };
 
 /*
